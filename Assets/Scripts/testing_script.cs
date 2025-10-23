594ffd487642a76ffe76644bf16cae71a871a28c
n1i9 <n.bosch.lsgi@gmail.com>
2025-10-23 16:15:25 +0200

    Pushing all the changes

--- Assets/Scripts/testing_script.cs
+++ Assets/Scripts/testing_script.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
-public class testing_script : MonoBehaviour
+public class Thisesting_script : MonoBehaviour
 {
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.ToString();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
