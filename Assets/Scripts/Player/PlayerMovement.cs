3d1d87f32c5e11551d13e0e6d3aaed9c0443d1b1
agente-E <alexpalmada89@gmail.com>
2025-11-19 17:15:34 +0100

    Studied how gravity should work

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,14 @@
-using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour {
     private readonly float _maxForwardSpeed = 5f;
     private readonly float _maxMovementSpeed = 4f;
+    private readonly float _jumpForce = 20f;
 
-    private readonly float _jumpForce = 2f;
-    private readonly float _gravityValue = -9.81f; // * This is actually the gravity we have on earth
-
-    private float _currentSpeed = 0f;
+    // ! WON'T BE SERIALIZED
+    [SerializeField]
+    private float _gravityForce = -9.81f; // * This is actually the gravity we have on earth
 
     [SerializeField, Tooltip("The amount to multiply the speed every frame, to get a smooth speed movement")]
     private float _speedMultiplier;
@@ -17,26 +16,23 @@ public class PlayerMovement : MonoBehaviour {
     private float _speedDecreaser;
     [SerializeField, Tooltip("The amount of speed the player will have when starts moving")]
     private float _initialSpeed;
+    private float _currentSpeed = 0f;
+    private float _verticalVelocity = 2f;
 
     private InputManager _inputManager;
-    private Rigidbody _rigidbody;
+    private CharacterController _characterController;
 
     [SerializeField, Tooltip("Add here the orientation object from the player")]
     private Transform _orientation;
 
-    private bool _isGrounded;
-
-    private void Awake() => _rigidbody = GetComponent<Rigidbody>();
+    private void Awake() => _characterController = GetComponent<CharacterController>();
 
     private void Start() => _inputManager = InputManager.Instance;
 
     private void FixedUpdate() => Move();
 
     private void Update() {
-        Move();
-        ApplyGravity();
-
-        if (_inputManager.JumpedThisFrame() && _isGrounded) { Jump(); }
+        Debug.Log(_characterController.isGrounded);
     }
 
     /// <summary>
@@ -45,30 +41,24 @@ public class PlayerMovement : MonoBehaviour {
     /// <returns>The player's current speed in units per second.</returns>
     public float GetCurrentSpeed() => _currentSpeed;
 
-    private void CheckGrounded() {
-        // TODO check when the player is touching the ground (Because characterController sucks)
-    }
-
-    private void ApplyGravity() {
-        if (!_isGrounded) {
-            // TODO Apply gravity to the character
-        }
-    }
-
-    private void Jump() {
-        // TODO implement logic to make the player jump
-        Debug.Log("Jumping");
+    private float ApplyGravity() {
+        return 2f;
     }
 
     private void Move() {
         // TODO Implement movement related with camera and make it move smoothly, make it still moving even when stopped until speed reaches 0
+
         // Get the player inputs into a vector
         var axis = _inputManager.GetPlayerMovement();
 
-        var moveDirection = _orientation.forward * axis.x + _orientation.right * axis.y;
+        // Get where the camera is looking at horizontally (So if the player looks vertically, it won't stop moving)
+        var moveDirection = (_orientation.forward * axis.y) + (_orientation.right * axis.x);
 
-        // Get what direction is the camera looking at, and normalize it so it doesn't tilt
-        var forwardDirection = Camera.main.transform.forward;
+        // TODO Add gravity to the current moveDirection var
+        moveDirection = moveDirection.normalized;
+
+        // Move the player
+        _characterController.Move(_initialSpeed * Time.deltaTime * moveDirection);
     }
 
     private void IncreaseSpeed() {
